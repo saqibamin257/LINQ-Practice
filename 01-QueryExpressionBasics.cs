a73f99b32389266d180182e1bb456dd840bc7ab7
Saqib Amin <saqibamin257@gmail.com>
2023-07-17 14:43:52 +0500

    Add project files.

---
+++ 01-QueryExpressionBasics.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQPractice
+{
+    public class QueryExpressionBasics
+    {
+        public int[] Scores = new int[] { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+        
+        public void Highscores_DescendingOrder() 
+        {
+            IEnumerable<int> highScore = from score in Scores
+                                         where score > 80
+                                         orderby score descending
+                                         select score;
+            foreach(var s in highScore)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LINQPractice;
+
+QueryExpressionBasics obj = new QueryExpressionBasics();
+obj.Highscores_DescendingOrder();
